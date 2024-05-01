12c5da70be517c63ce86306cb2e88361c594e598
Alejandro <alexavdstiles@gmail.com>
2024-04-30 20:47:05 -0600

    initialize image recognition on torch

---
+++ imagesRecognition_workspace/torch/Torch.py
@@ -0,0 +1,29 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from os import path
+
+DATA_DIR = path.join(path.dirname(path.abspath(__file__)), 'data')
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root=DATA_DIR, train=True,
+                                        download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root=DATA_DIR, train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
